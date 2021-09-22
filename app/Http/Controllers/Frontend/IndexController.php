0ccebf56e66b2b8e53dcdff397a44363b3b5d606
asif <aasif5644@gmail.com>
2021-09-22 20:03:56 +0600

    cat wise product show done in All products

--- app/Http/Controllers/Frontend/IndexController.php
+++ app/Http/Controllers/Frontend/IndexController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
-
 use App\Models\Product;
 use App\Models\MultiImg;
 use App\Models\Slider;
@@ -15,10 +14,11 @@ class IndexController extends Controller
 {
     public function index()
     {
+        $products=Product::where('status',1)->orderBy('id','DESC')->get();
         $categories=Category::orderBy('category_name_en','ASC')->get();
         /* limit(5) or we can use take() function aswell */
         $sliders=Slider::where('status',1)->orderBy('id','DESC')->limit(5)->get();
-        return view('frontend.index',compact('categories','sliders'));
+        return view('frontend.index',compact('categories','sliders','products'));
     }
 
     public function singleproduct($id, $slug)
